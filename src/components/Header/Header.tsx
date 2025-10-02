1d667660913531999d738266be4e2c2706464826
Anna Kusmina <anna.kusmina.11.11@gmail.com>
2025-10-02 16:41:43 +0200

    main page, character page

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,10 @@
+import style from "./Header.module.css";
+
+
+const Header = () => {
+
+    return (
+        <h1 className={style["header"]}>CHARACTERS FROM RICK AND MORTY</h1>
+    )
+}
+export default Header;
